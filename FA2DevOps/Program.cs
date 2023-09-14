75dc972bc22b5987945d67ecdb2017fd5d0e6f7e
Olwethu22 <olwethuketwa@gmail.com>
2023-09-14 12:46:19 +0200

    added line 4

--- FA2DevOps/Program.cs
+++ FA2DevOps/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.ReadLine();
+Console.WriteLine("What is your name");
+Console.ReadLine();
